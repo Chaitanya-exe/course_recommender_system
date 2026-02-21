84324fbf816004c211cbbcefb45ca43385641976
Chaitanya-exe <jeetkrishna3333@gmail.com>
2026-02-21 17:47:15 +0530

    student profile dataset generation pipeline init

---
+++ recommender_system/__init__.py
@@ -0,0 +1,2 @@
+from .eligibility_pipeline import StudentProfile
+__all__ = ["StudentProfile"]
--- recommender_system/eligibility_pipeline.py
+++ recommender_system/eligibility_pipeline.py
@@ -1,3 +1,18 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class StudentProfile(BaseModel):
+    degree_level: str = Field(description="student's current degree level")
+    percentage: float = Field(description="student's latest percentage in the degree")
+    is_reserved: bool = Field(desctiption="whether student belong to a reserved category or not")
+    academic_background: str = Field(description="student's degree name and subject")
+    subjects: List[str] = Field(description="subjects studied in the course")
+    interests: List[str] = Field(description="student's area of interests")
+    career_goal: str = Field(desctiption="student's career goal")
+    preferred_skills: List[str] = Field(description="student's preferred skills to learn")
+    preferred_domain: str = Field(description="student's preferred domain")
+
+
 class EligibilityRule:
 
     def __init__(self):
@@ -7,7 +22,6 @@ class EligibilityRule:
             "PG" : 2,
             "PhD" : 3
         }                            
-
-    @staticmethod
-    def is_degree_matching():
+    
+    def is_degree_matching(self, student_profile):
         pass
