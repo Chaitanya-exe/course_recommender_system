874540c48ab8075e9bcdae974b87b7fde58bfa3d
Chaitanya-exe <jeetkrishna3333@gmail.com>
2026-03-18 01:53:50 +0530

    first recommendation ranking

--- recommender_system/main.py
+++ recommender_system/main.py
@@ -2,6 +2,7 @@ from eligibility_pipeline import EligibilityRule
 import pandas as pd
 import re
 from vectorizer import TFIDFVector, calculate_similarity
+import numpy as np
 
 def parse_eligibility_struct(text):
     degree = re.search(r"min_degree_level='(.*?)'", text)
@@ -13,38 +14,41 @@ def parse_eligibility_struct(text):
         "min_marks_general": float(gen.group(1)) if gen else None,
         "min_marks_reserved": float(res.group(1)) if res else None
     }
-
+recommendation_df = pd.DataFrame()
 def main():
     students_df = pd.read_csv("recommender_system/students_data.csv")
     courses_df = pd.read_csv("recommender_system/final_data3.csv")
     eligibility_pipe = EligibilityRule()
     tfidf = TFIDFVector()
-
-    results = []
+    courses_text = [f"""{course['program_name']} {course['domain']} {course['description']} {" ".join(course['skills_learned'])} {" ".join(course['career_outcomes'])}""" for course in courses_df.iterrows()]
+    tfidf.fit(courses_text)
 
     for (i, student) in students_df.iterrows():
-        eligible_courses_text = []
+        eligible_courses = []
+
         for (j, course) in courses_df.iterrows():
             try:
                 result = eligibility_pipe.check_eligibility(student, course)
 
                 if result[0]:
-                    text = f"""{course['program_name']} {course['domain']} {course['description']} {" ".join(course['skills_learned'])} {" ".join(course['career_outcomes'])}"""
-                    eligible_courses_text.append(text)
+
+                    eligible_courses.append(course)
                 else:
                     continue
             except:
                 print(f"Some problem with this course {course["program_name"]}")
                 continue
+        eligible_courses_text = [f"""{course['program_name']} {course['domain']} {course['description']} {" ".join(course['skills_learned'])} {" ".join(course['career_outcomes'])}""" for course in eligible_courses]
+
         print(f"Student {i + 1} eligible for {len(eligible_courses_text)} courses.")
         student_text = f"""{student['academic_background']} {student['subjects']} {student['preferred_skills']} {student['preferred_domain']} {student['career_goal']} {student['interests']}"""
-        tfidf.fit(eligible_courses_text)
         course_matrix = tfidf.transform(eligible_courses_text)
         student_vector = tfidf.transform(student_text)
 
         scores = calculate_similarity(student_vector=[student_vector], courses_matrix=course_matrix)
-        
-        print(course_matrix.shape)
+        top_indices = np.argsort(scores)[::1][:7]
+        recommended_courses = [eligible_courses[i] for i in top_indices]
+
 
         
         
